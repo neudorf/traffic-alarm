f45622e6eaa0acde1114faf1b55a83e2b2c7e9e7
Josh Neudorf <josh.c.neudorf@gmail.com>
2015-08-24 14:53:09 -0600

    added javascript web app version of program and moved tkinter python version to separate folder

---
+++ javascript-web-app/script.js
@@ -0,0 +1,41 @@
+function update(){
+    var now = new Date();
+    var bedHour = parseInt($('#bedTimeInput').val().slice(0,2));
+    var wakeHour = parseInt($('#wakeTimeInput').val().slice(0,2));
+    var bedMin = parseInt($('#bedTimeInput').val().slice(3,5));
+    var wakeMin = parseInt($('#wakeTimeInput').val().slice(3,5));
+    console.log("bedHour: " + bedHour + "wakeHour: "+wakeHour+"bedMin: "+ bedMin + "wakeMin: "+wakeMin + '\n' + "now Hour: " + now.getHours() + "now Minute: "+now.getMinutes())
+    if (bedHour > wakeHour){ //night time, sleeping overnight
+            bed = !(((now.getHours() < bedHour) && (now.getHours() > wakeHour)) || (now.getHours()==bedHour && now.getMinutes() < bedMin) || (now.getHours()==wakeHour && now.getMinutes() >= wakeMin));
+    }
+    else if (bedHour < wakeHour){ //nap time, sleeping during the day
+            bed = ((now.getHours() < wakeHour) && (now.getHours() > bedHour)) || (now.getHours()==bedHour && now.getMinutes() >= bedMin) || (now.getHours()==wakeHour && now.getMinutes() < wakeMin);
+    }
+    else if (bedHour == wakeHour){ //nap time, sleeping during the day
+            bed = (now.getMinutes() >= bedMin && now.getMinutes() < wakeMin);
+    }
+    //console.log("now="+now+"bedTime="+bedTime+"wakeTime="+wakeTime)
+    if(bed){
+        document.getElementById("stoplight").src="stoplightRed.png";
+        console.log("Bedtime? True");
+    }
+    else{
+        document.getElementById("stoplight").src="stoplightGreen.png";
+        console.log("Bedtime? False");
+    }
+};
+
+$(document).ready(function(){
+    jQuery('#bedTimeInput').datetimepicker({
+      datepicker:false,
+      format:'H:i',
+      step:15
+    });
+    jQuery('#wakeTimeInput').datetimepicker({
+      datepicker:false,
+      format:'H:i',
+      step:15
+    });
+    update();
+    setInterval('update()',5000);
+});
